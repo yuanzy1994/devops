be124d4a24c5d338b7344768865dab354211da1e
yuanzy <yzy216@qq.com>
2017-07-04 10:07:41 +0800

    20%

--- zabbix-dev/tmp.py
+++ zabbix-dev/tmp.py
@@ -1,1 +1,28 @@
-a
+[
+    {
+    u'host': u'Zabbix server',
+     u'hostid': u'10084',
+    u'interfaces': [
+        {
+          u'interfaceid': u'1',
+          u'ip': u'127.0.0.1'
+        }
+                   ]
+    },
+
+{u'host': u'test-Basic', u'hostid': u'10105', u'interfaces': [{u'interfaceid': u'2', u'ip': u'172.16.80.25'}]},
+
+{u'host': u'test-tuia', u'hostid': u'10115', u'interfaces': [{u'interfaceid': u'3', u'ip': u'172.16.80.61'}]}, {u'host': u'test-duiba',
+
+ u'hostid': u'10116', u'interfaces': [{u'interfaceid': u'4', u'ip': u'172.16.80.62'}]}, {u'host': u'test-maila',
+
+ u'hostid': u'10117', u'interfaces': [{u'interfaceid': u'5', u'ip': u'172.16.80.63'}]}, {u'host': u'test-qiho',
+
+ u'hostid': u'10118', u'interfaces': [{u'interfaceid': u'6', u'ip': u'172.16.80.60'}]}, {u'host': u'test-base-service',
+
+ u'hostid': u'10119', u'interfaces': [{u'interfaceid': u'7', u'ip': u'172.16.80.64'}]}, {u'host': u'test-jenkins',
+
+  u'hostid': u'10126', u'interfaces': [{u'interfaceid': u'8', u'ip': u'172.16.80.21'}]}, {u'host': u'dev-jenkins',
+
+  u'hostid': u'10131', u'interfaces': [{u'interfaceid': u'11', u'ip': u'192.168.2.36'}]}
+  ]
